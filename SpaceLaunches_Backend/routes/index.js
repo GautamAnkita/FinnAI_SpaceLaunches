d4455364cb096f6365d48bb25035bad2af470771
AnkitaGautam-phsa <ankita.gautam@phsa.ca>
2019-04-08 22:22:55 -0700

    Initial code formatting and cleanup

--- SpaceLaunches_Backend/routes/index.js
+++ SpaceLaunches_Backend/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("Welcome");
+  res.status(200).json();
 });
 
 module.exports = router;
