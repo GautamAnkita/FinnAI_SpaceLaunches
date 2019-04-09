a4e008557e36912c330e9b8e091ab73bb2e80a6c
GautamAnkita <reply.ankita@gmail.com>
2019-04-09 11:54:46 -0700

    Add API integration with backend

--- spacelaunches_frontend/src/components/SpaceLaunchesHome.jsx
+++ spacelaunches_frontend/src/components/SpaceLaunchesHome.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
+import LaunchLibrary from "../requests/launchlibrary";
 
 class SpaceLaunchesHome extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            loading: true,
+            spaceLaunches: []
+        };
+    }
+    
+    componentDidMount(){
+        LaunchLibrary.getUpcomingLaunches()
+        .then(response => {
+            console.log("Received response:"+ JSON.stringify(response));
+            this.setState({ loading: false, spaceLaunches: response });
+        })
+        .catch(() => {
+            this.setState({ loading: false });
+        });
+    }
     render(){
+        const { loading, spaceLaunches } = this.state;
+        if(loading){
+            return(
+                <main>
+                    <h2>Loading...</h2>
+                </main>
+            );
+        }
         return(
                 <div>
                     <h1>Welcome</h1>
+                    <h5>{spaceLaunches}</h5>
                 </div>
         );
     }
---
+++ spacelaunches_frontend/src/requests/config.js
@@ -0,0 +1,5 @@
+export const HOST = "localhost:3000";
+export const BASE_URL = `http://${HOST}`;
+
+
+
